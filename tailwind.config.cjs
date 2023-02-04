26e5cd3bb205d218e89b8d7f82de8d088cb94e94
yRicardinBaum <ricardinbaum@gmail.com>
2023-02-04 12:46:31 -0300

    Commit2

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./node_modules/flowbite-react/**/*.js",
+      "./src/**/*.{html,js,svelte,ts,tsx,jsx}"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+}
