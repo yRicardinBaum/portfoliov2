ac862723bde4e303fe8f1966c7a22d9c11545b06
yRicardinBaum <ricardinbaum@gmail.com>
2023-02-04 14:10:05 -0300

    Commit2

---
+++ components/Header.jsx
@@ -0,0 +1,46 @@
+import {Component} from "react";
+import {Dropdown, Navbar} from "flowbite-react";
+import Image from "next/image";
+
+
+
+export default class NavBarMenu extends Component{
+    render() {
+        return (
+            <>
+                <Navbar
+                    fluid={true}
+                    rounded={true}
+                >
+                    <Navbar.Brand>
+                        <Image src="/images/logo-sesi.png" className="w-[60px] h-[30px] md:h-[40px] md:w-[110px]" alt="Sesi Logo" height={100} width={100}/>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className={"pr-10"} >
+                        <Navbar.Link href="/" className={"text-blue-600"}>Home</Navbar.Link>
+                        <Dropdown label="Matérias" className={"transition"} inline={true}>
+                            <Dropdown.Header>
+                                <span className="block text-sm">Minhas Matérias</span>
+                            </Dropdown.Header>
+                            <Dropdown.Item>
+                                Linguagens
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Matemática
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Ciência da Natureza
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Ciências Humanas
+                            </Dropdown.Item>
+                        </Dropdown>
+                    </Navbar.Collapse>
+                </Navbar>
+            </>
+        )
+    }
+}
+
+
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,18 @@
-import { Dropdown, Navbar } from "flowbite-react";
-import Image from "next/image";
+import NavBarMenu from "../../components/Header";
+import { useState } from "react";
+import AlertManeiro from "../../components/Alert.jsx";
 
 export default function Home() {
-  // @ts-ignore
+    const [visible] = useState(false);
+
     return (
     <>
       <main>
-        <header className={"p-5"}>
-          <Navbar
-              fluid={true}
-              rounded={true}
-          >
-              <Navbar.Brand>
-                  <Image src="/images/logo-sesi.png" className="w-[60px] h-[30px] md:h-[40px] md:w-[110px]" alt="Sesi Logo" height={100} width={100}/>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-              <Navbar.Collapse className={"pr-10"} >
-                  <Navbar.Link href="/" active={true}>Home</Navbar.Link>
-                  <Dropdown label="Matérias" className={"transition"} inline={true}>
-                      <Dropdown.Header>
-                        <span className="block text-sm">Minhas Matérias</span>
-                      </Dropdown.Header>
-                      <Dropdown.Item>
-                          Linguagens
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                          Matemática
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                          Ciência da Natureza
-                      </Dropdown.Item>
-                      <Dropdown.Item>
-                          Ciências Humanas
-                      </Dropdown.Item>
-                  </Dropdown>
-              </Navbar.Collapse>
-          </Navbar>
-        </header>
+        <AlertManeiro/>
 
+        <header className={"p-5"} hidden={visible}>
+            <NavBarMenu/>
+        </header>
 
       </main>
     </>
