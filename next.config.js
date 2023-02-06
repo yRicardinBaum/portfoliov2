6f4a8b62023618f995bf2305f696bdc97023a950
yRicardinBaum <ricardinbaum@gmail.com>
2023-02-05 22:01:35 -0300

    Fixing Bugs

--- next.config.js
+++ next.config.js
@@ -2,11 +2,6 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  "extends": "next",
-  "rules": {
-    "react/no-unescaped-entities": "off",
-    "@next/next/no-page-custom-font": "off"
-  }
 }
 
 module.exports = nextConfig
